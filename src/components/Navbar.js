45b2a1c983a35faee517c9836c58f7cfcceb9d1b
Gerardo Solis <gsolis5114@gmail.com>
2024-07-03 20:24:04 -0700

    Updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,14 +7,26 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/posts">Posts</a></li>
+                <li>
+                    <a
+                        href="/" className="home-link">
+                        <span className="home-text">Home</span>
+                        <span className="home-button"/>
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="/posts" className="post-link">
+                        <span className="post-text">Posts</span>
+                        <span className="post-button"/>
+                    </a>
+                </li>
                 <li><a href="/about">About Me</a></li>
                 <li><a href="/contact">Contact</a></li>
                 <li>
                     <a href="/search" className="search-link">
                     <span className="search-icon"/>
-                    <span className="search-icon-bg"></span>
+                    <span className="search-button"></span>
                     </a>
                 </li>
             </ul>

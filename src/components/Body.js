e8461aaf0b337d1e322596de0d5ca23b02e78cf8
Gerardo Solis <gsolis5114@gmail.com>
2024-07-28 08:49:45 -0700

    Added name-tag

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,6 +3,7 @@ import "../styles/component_styles/Body.css";
 import BG_Portrait from "../svg/BG_Portrait";
 import BG_Square from "../svg/BG_Square";
 import portrait from "../assets/body/Portrait.png";
+import { ReactComponent as Name_Tag } from "../assets/body/Name_Tag.svg";
 
 const Body = () => {
   return (
@@ -10,14 +11,23 @@ const Body = () => {
       <div className="bg-container">
         {/* eslint-disable-next-line react/jsx-pascal-case */}
         <BG_Square />
-        <div className="portrait-container">
-          {/* eslint-disable-next-line react/jsx-pascal-case */}
-          <BG_Portrait />
-          <img
-            className="portrait-img"
-            src={portrait}
-            alt={"Gerardo Solis Portrait"}
-          />
+      </div>
+      <div className="portrait-container">
+        {/* eslint-disable-next-line react/jsx-pascal-case */}
+        <BG_Portrait />
+        <img
+          className="portrait-img"
+          src={portrait}
+          alt={"Gerardo Solis Portrait"}
+        />
+      </div>
+      <div className="name-tag">
+        <Name_Tag />
+        <div className="name-tag-content">
+          <div className="name-tag-text">Gerardo Solis</div>
+          <div className="name-tag-subtext">
+            Software Engineering | Offensive Security | Game Development
+          </div>
         </div>
       </div>
     </div>

b90e0c7520fa7e067a9fef9d0d8d1eccb08a9930
Gerardo Solis <gsolis5114@gmail.com>
2024-08-20 07:41:40 -0700

    Added Footer Box Content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "../styles/component_styles/Footer.css";
 import FooterBlock from "../svg/FooterBlock";
+import Pdf from "../assets/docs/Gerardo-Solis-Resume.pdf";
 
 const Footer = () => {
   return (
     <div className="body">
       <footer className="footer-container">
         <div className="footer-block-container">
-          <div className="footer-block">
+          <div className="footer-block-1">
+            <div className="footer-content">
+              <text>Certifications</text>
+              <a href={Pdf} target="_blank" className="resume-link">
+                Resume
+              </a>
+              <text>gsolis5114@gmail.com</text>
+            </div>
             <FooterBlock />
           </div>
           <div className="footer-block">

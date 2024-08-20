9f66554cff2164f3036b53f45e3bd2b849be0a79
Gerardo Solis <gsolis5114@gmail.com>
2024-08-20 07:57:40 -0700

    Updated footer box1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import React from "react";
 import "../styles/component_styles/Footer.css";
 import FooterBlock from "../svg/FooterBlock";
 import Pdf from "../assets/docs/Gerardo-Solis-Resume.pdf";
+import fileIcon from "../assets/footer/file.png";
+import mentionIcon from "../assets/footer/mention.png";
 
 const Footer = () => {
   return (
@@ -10,11 +12,26 @@ const Footer = () => {
         <div className="footer-block-container">
           <div className="footer-block-1">
             <div className="footer-content">
-              <text>Certifications</text>
-              <a href={Pdf} target="_blank" className="resume-link">
-                Resume
-              </a>
-              <text>gsolis5114@gmail.com</text>
+              <div className="horizontal-align">
+                <img src={fileIcon} alt="CertIcon" className="cert-icon" />
+                <a href="" target="_blank" className="cert-link">
+                  Certifications
+                </a>
+              </div>
+              <div className="horizontal-align">
+                <img src={fileIcon} alt="Resume Icon" className="resume-icon" />
+                <a href={Pdf} target="_blank" className="resume-link">
+                  Resume
+                </a>
+              </div>
+              <div className="horizontal-align">
+                <img
+                  src={mentionIcon}
+                  alt="Mention Icon"
+                  className="mention-icon"
+                />
+                <text>gsolis5114@gmail.com</text>
+              </div>
             </div>
             <FooterBlock />
           </div>

c27cd7113e8b7b9d806cf61f303bb9190610cfe1
Jerry Solis <jsouliss@users.noreply.github.com>
2026-06-05 04:13:10 -0700

    Added posts API route

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,17 @@
 import { Hono } from 'hono'
-const app = new Hono()
+import { Client } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres"
+import { postTable } from "./db/schema"
 
+type Bindings = {
+    HYPERDRIVE: Hyperdrive
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+// Access environment values
 app.get('/api/health', (c) => c.json({ status: "ok" }))
+
+app.get('/api/posts', (c) => c.json({   }))
 
 export default app

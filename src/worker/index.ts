22e7e7b1c5d9673d11c07a606af7889ad566fc13
Gerardo Solis <gsolis5114@gmail.com>
2026-06-05 08:23:45 -0700

    Added wrangler types

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,10 +1,14 @@
 import { Hono } from 'hono'
 import { Client } from "pg";
-import { drizzle } from "drizzle-orm/node-postgres"
-import { postTable } from "./db/schema"
+import { drizzle } from "drizzle-orm/node-postgres";
+import { postTable } from "./db/schema";
 
 type Bindings = {
     HYPERDRIVE: Hyperdrive
+}
+
+export async function getPosts(db) {
+    return db.select().from(postTable)
 }
 
 const app = new Hono<{ Bindings: Bindings }>()
@@ -12,6 +16,6 @@ const app = new Hono<{ Bindings: Bindings }>()
 // Access environment values
 app.get('/api/health', (c) => c.json({ status: "ok" }))
 
-app.get('/api/posts', (c) => c.json({   }))
+app.get('/api/posts', async (c) => c.json(await getPosts(db)))
 
 export default app

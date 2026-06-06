09d2b3e6d5ebcedd53bbdedd1220d7bbc034abbc
Jerry Solis <jsouliss@users.noreply.github.com>
2026-06-06 13:06:05 -0700

    Added posts API endpoint with Drizzle and Hyperdrive

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { Client } from "pg";
+import { NodePgDatabase} from "drizzle-orm/node-postgres";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { postTable } from "./db/schema";
 
@@ -7,7 +8,7 @@ type Bindings = {
     HYPERDRIVE: Hyperdrive
 }
 
-export async function getPosts(db) {
+export async function getPosts(db: NodePgDatabase) {
     return db.select().from(postTable)
 }
 
@@ -16,6 +17,29 @@ const app = new Hono<{ Bindings: Bindings }>()
 // Access environment values
 app.get('/api/health', (c) => c.json({ status: "ok" }))
 
-app.get('/api/posts', async (c) => c.json(await getPosts(db)))
+app.get('/api/posts', async (c) => {
+    // 1. Create a node-postgres client configured for Hyperdrive
+    const client = new Client({ connectionString: c.env.HYPERDRIVE.connectionString });
+
+    // 2. Connect the client
+    await client.connect();
+
+    // 3. Initialize Drizzle
+    const db = drizzle(client);
+
+    // 4. Use Drizzle
+    const result = await getPosts(db); 
+
+    // Remember to close the client if not reusing in a pool scenario
+    await client.end();
+
+    return c.json(result)
+})
+
+app.onError((err, c) => {
+    console.error(err)
+    return c.text('Custom Error Message', 500)
+})
+
 
 export default app
